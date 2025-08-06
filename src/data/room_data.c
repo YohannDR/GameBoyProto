ab5fdb39fee665ffc278a214d2a48b053e81e396
YohannDR <yohann.demaison-regne@orange.fr>
2025-08-06 17:47:35 +0200

    Change room origin formatting

--- src/data/room_data.c
+++ src/data/room_data.c
@@ -13,8 +13,8 @@ const struct RoomInfo sRooms[] = {
         .spriteData = sRoom0_SpriteData,
         .doorData = sRoom0_DoorData,
         .collisionTable = 0,
-        .originX = 0,
-        .originY = 0
+        .originX = SCREEN_SIZE_X_SUB_PIXEL * 0,
+        .originY = SCREEN_SIZE_X_SUB_PIXEL * 0
     },
     [1] = {
         .tilemap = sRoom1_Tilemap,
@@ -23,6 +23,6 @@ const struct RoomInfo sRooms[] = {
         .doorData = sRoom1_DoorData,
         .collisionTable = 0,
         .originX = SCREEN_SIZE_X_SUB_PIXEL * 2,
-        .originY = 0
+        .originY = SCREEN_SIZE_X_SUB_PIXEL * 0
     }
 };
