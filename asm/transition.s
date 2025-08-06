847a8ab1af49d739a05923fd378bccb2814aa745
YohannDR <yohann.demaison-regne@orange.fr>
2025-08-07 01:04:37 +0200

    Remove tabs in transition.s

--- asm/transition.s
+++ asm/transition.s
@@ -1,19 +1,19 @@
-	.module tilemap
+    .module tilemap
 
     .globl _UpdateTransitionVblank
 
 _UpdateTransitionVblank:
-	ld	a, (#_gDoorTransition)
-	or	a, a
+    ld a, (#_gDoorTransition)
+    or a, a
     ret Z
 
-	ld	a, (#_gDoorTransition)
+    ld a, (#_gDoorTransition)
     dec a
-    jr  Z, UpdateNormalTransition
+    jr Z, UpdateNormalTransition
 
-	ld	a, (#_gDoorTransition)
+    ld a, (#_gDoorTransition)
     sub a, #0x02
-    jr  Z, UpdateLoadingTransition
+    jr Z, UpdateLoadingTransition
 
 UpdateNormalTransition:
     ret
