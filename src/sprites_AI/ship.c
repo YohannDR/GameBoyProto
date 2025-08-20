10a733597eeb43bcb1400c25b906f01e64838355
chezmoa <chez.moa@hotmail.fr>
2025-08-20 17:53:57 +0200

    Add proper initialization ship AI

--- src/sprites_AI/ship.c
+++ src/sprites_AI/ship.c
@@ -6,10 +6,27 @@
 
 #define MOVING_SPEED (PIXEL_SIZE)
 
+extern const struct AnimData sShipAnim_Idle[];
+
 void Ship(void)
 {
-    // TODO proper initialization (setting animation, draw distance, X flipping if going right and pose 1)
+    // proper initialization (setting animation, draw distance, X flipping if going right and pose 1)
+    if (gCurrentSprite.pose == 0)
+    {   
+        gCurrentSprite.animPointer = sShipAnim_Idle;
+        gCurrentSprite.pose = 1;
 
+        gCurrentSprite.drawDistanceLeft = SUB_PIXEL_TO_PIXEL(0);
+        gCurrentSprite.drawDistanceRight = SUB_PIXEL_TO_PIXEL(BLOCK_SIZE);
+        gCurrentSprite.drawDistanceTop = -SUB_PIXEL_TO_PIXEL(BLOCK_SIZE);
+        gCurrentSprite.drawDistanceBottom = SUB_PIXEL_TO_PIXEL(0);
+
+        if (gCurrentSprite.properties == SPRITE_PROPERTY_X_FLIP)
+        {
+            gCurrentSprite.part = SHIP_RIGHT;
+        }
+        
+    }
     // TODO proper screen looping? This loops via underflow/overflow which is a bit weird
     if (gCurrentSprite.part == SHIP_LEFT)
         gCurrentSprite.x -= MOVING_SPEED;
