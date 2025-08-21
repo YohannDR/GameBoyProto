6c4fc08baa101c16dca5ebba789763e763518b7e
5tel <estelle.harle.3@gmail.com>
2025-08-21 13:01:47 +0200

    Update - Room 17 - 18

---
+++ include/data/rooms/room17.h
@@ -0,0 +1,10 @@
+#ifndef ROOM_17_H
+#define ROOM_17_H
+
+#include "room.h"
+
+extern const u8 sRoom17_Tilemap[];
+extern const struct RoomSprite sRoom17_SpriteData[];
+extern const u8 sRoom17_DoorData[];
+
+#endif /* ROOM_17_H */
