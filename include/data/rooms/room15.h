0cad803a496b33911677d91322f3fa9d5f0d22a1
5tel <estelle.harle.3@gmail.com>
2025-08-21 12:27:32 +0200

    Update Room 14 - 15 - 16

---
+++ include/data/rooms/room15.h
@@ -0,0 +1,10 @@
+#ifndef ROOM_15_H
+#define ROOM_15_H
+
+#include "room.h"
+
+extern const u8 sRoom15_Tilemap[];
+extern const struct RoomSprite sRoom15_SpriteData[];
+extern const u8 sRoom15_DoorData[];
+
+#endif /* ROOM_15_H */
---
+++ include/data/rooms/room16.h
@@ -0,0 +1,10 @@
+#ifndef ROOM_16_H
+#define ROOM_16_H
+
+#include "room.h"
+
+extern const u8 sRoom16_Tilemap[];
+extern const struct RoomSprite sRoom16_SpriteData[];
+extern const u8 sRoom16_DoorData[];
+
+#endif /* ROOM_16_H */
